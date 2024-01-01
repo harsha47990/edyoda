b64a63786a37b74be2e8a5e7cf993cfeea789862
harsha47990 <88038908+harsha47990@users.noreply.github.com>
2024-01-02 01:16:07 +0530

    Update pytorch_simple_regression.py

--- Deep Learning/pytorch_simple_regression.py
+++ Deep Learning/pytorch_simple_regression.py
@@ -1,3 +1,5 @@
+#code 1
+
 import torch
 
 X = torch.tensor([1, 2, 3, 4], dtype=torch.float32)
@@ -36,3 +38,64 @@ for epoch in range(n_iters):
 
     if epoch % 10 == 0:
         print(f'epoch {epoch+1}: w = {w.item():.3f}, loss = {l.item():.8f}')
+
+
+
+#code 2
+
+import torch
+import math
+torch.manual_seed(0)
+dtype = torch.float
+device = torch.device("cpu")
+# device = torch.device("cuda:0")  # Uncomment this to run on GPU
+
+# Create Tensors to hold input and outputs.
+# By default, requires_grad=False, which indicates that we do not need to
+# compute gradients with respect to these Tensors during the backward pass.
+x = torch.linspace(-math.pi, math.pi, 2000, device=device, dtype=dtype)
+y = torch.sin(x)
+
+# Create random Tensors for weights. For a third order polynomial, we need
+# 4 weights: y = a + b x + c x^2 + d x^3
+# Setting requires_grad=True indicates that we want to compute gradients with
+# respect to these Tensors during the backward pass.
+a = torch.randn((), device=device, dtype=dtype, requires_grad=True)
+b = torch.randn((), device=device, dtype=dtype, requires_grad=True)
+c = torch.randn((), device=device, dtype=dtype, requires_grad=True)
+d = torch.randn((), device=device, dtype=dtype, requires_grad=True)
+
+learning_rate = 1e-6
+for t in range(2000):
+    # Forward pass: compute predicted y using operations on Tensors.
+    y_pred = a + b * x + c * x ** 2 + d * x ** 3
+
+    # Compute and print loss using operations on Tensors.
+    # Now loss is a Tensor of shape (1,)
+    # loss.item() gets the scalar value held in the loss.
+    loss = (y_pred - y).pow(2).sum()
+    if t % 100 == 99:
+        print(id(a))
+        print(t, loss.item())
+
+    # Use autograd to compute the backward pass. This call will compute the
+    # gradient of loss with respect to all Tensors with requires_grad=True.
+    # After this call a.grad, b.grad. c.grad and d.grad will be Tensors holding
+    # the gradient of the loss with respect to a, b, c, d respectively.
+    loss.backward()
+    # Manually update weights using gradient descent. Wrap in torch.no_grad()
+    # because weights have requires_grad=True, but we don't need to track this
+    # in autograd.
+
+    a = a - learning_rate * a.grad
+    b = b - learning_rate * b.grad
+    c = c - learning_rate * c.grad
+    d = d - learning_rate * d.grad
+    # Manually zero the gradients after updating weights
+    a.grad = None
+    b.grad = None
+    c.grad = None
+    d.grad = None
+
+print(f'Result: y = {a.item()} + {b.item()} x + {c.item()} x^2 + {d.item()} x^3')
+
